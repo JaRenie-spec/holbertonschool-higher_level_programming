7e59e1209628f9196a79d166def87500c3632723
JaRenie-spec <renie.jeanalain@gmail.com>
2025-01-20 13:53:50 +0100

    correction syntax

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
-        print(" ".join("{}".format(num) for num in x))
+        print(" ".join("{:d}".format(num) for num in x))
