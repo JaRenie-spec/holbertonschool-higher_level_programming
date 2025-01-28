ba8b742896dce1df95c6539a38508bf2d7725c37
JaRenie-spec <renie.jeanalain@gmail.com>
2025-01-28 13:03:25 +0100

    add task 0 to 3 + README

---
+++ python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""program that create a class
+"""
+class Square:
+    """an empty square that we will define later
+    """
---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""program that create a class
+"""
+class Square:
+    """an empty square that we will define later
+    """
+    def __init__(self, size):
+        self.__size = size
