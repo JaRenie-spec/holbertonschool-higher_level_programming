a9fdc43f5e23b893c7cfa2c17065ed38772ec202
JaRenie-spec <renie.jeanalain@gmail.com>
2025-02-04 09:40:03 +0100

    adding task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+
+"""define a function that checks if an object is a subclass inherited
+from a specified class."""
+
+
+def inherits_from(obj, a_class):
+    return issubclass(int, a_class)
