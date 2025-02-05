1affc9511a4d9a665bac60869a2256dfaa6f28fd
JaRenie-spec <renie.jeanalain@gmail.com>
2025-02-05 09:41:44 +0100

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module defining the BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """A base class for geometric operations."""
+
+    def area(self):
+        """Raises Exception indicating that the method is not implemented."""
+        raise Exception("area() is not implemented")
