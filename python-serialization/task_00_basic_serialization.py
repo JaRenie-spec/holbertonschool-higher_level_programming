e854396f0eda8d33df7c09a5fc45bd8806e165e0
JaRenie-spec <renie.jeanalain@gmail.com>
2025-02-11 13:21:54 +0100

    fix documentation

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
+"""initialize and empty dictionary to be serialized"""
+
+
 import json
 
-"""initialize and empty dictionary to be serialized"""
 data = {}
+
 
 def serialize_and_save_to_file(data, filename):
     """serialize and save data to the specified file"""
     with open(filename, "w") as file:
         json.dump(data, file)
 
+
 serialize_and_save_to_file(data, 'data.json')
+
 
 def load_and_deserialize(filename):
     """load and deserialize data from the specified file"""
     with open(filename, "r") as file:
         data_un = json.load(file)
         print(data_un)
+
+
 load_and_deserialize('data.json')
