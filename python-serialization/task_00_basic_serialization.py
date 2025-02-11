264d679be3dfbae9d12e779a931f3e5b9c885723
JaRenie-spec <renie.jeanalain@gmail.com>
2025-02-11 13:27:31 +0100

    add print

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -11,7 +11,7 @@ def serialize_and_save_to_file(data, filename):
     """serialize and save data to the specified file"""
     with open(filename, "w") as file:
         json.dump(data, file)
-
+        print("Data serialized and saved to 'data.json'.")
 
 serialize_and_save_to_file(data, 'data.json')
 
@@ -19,8 +19,9 @@ serialize_and_save_to_file(data, 'data.json')
 def load_and_deserialize(filename):
     """load and deserialize data from the specified file"""
     with open(filename, "r") as file:
-        data_un = json.load(file)
-        print(data_un)
+        deserialized_data = json.load(file)
+        print("Deserialized Data:")
+        print(deserialized_data)
 
 
 load_and_deserialize('data.json')
